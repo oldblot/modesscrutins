3e3a0ec0c169ed402ff6d8fdc84db6b0856894a4
Arnaud <arno1erdu8@gmail.com>
2022-09-14 22:43:12 +0200

    [ADD] | good print

--- scrutin5.py
+++ scrutin5.py
@@ -1,38 +1,39 @@
 import csv
-from unittest import result
 #def scrutin1():
-listCandidat1 = []
 listCandidat2 = []
+listCandidat1 = []
 with open("tableauvote.csv", newline='',encoding='utf-8') as csvfile:
     tableau = csv.reader(csvfile)
     tab = list(tableau)
-for ligne in range(len(tab)-1):
-    for case in tab: 
-        #print(case[ligne])
-        if ligne>1:
-            #print("case2 = ", case[ligne])
-            #print("case1 = ", case[ligne-1])
-            if (case[ligne] > case[ligne-1]):
-                listCandidat2.append(case[0])
+for index in range(len(tab)-1):
+    #print(index)
+    for Ligne in tab:
+        #print(Ligne, "n")
+        #print(Ligne[index])
+        if index>1:
+            #print("Element Ligne2 = ", Ligne[index])
+            #print("Element Ligne1 = ", Ligne[index-1])
+            if (Ligne[index] > Ligne[index-1]):
+                listCandidat1.append(Ligne[0])
             else:
-                listCandidat1.append(case[0])
-    #print("List1 = ", listCandidat1)
-    #print("List2 = ", listCandidat2)
+                listCandidat2.append(Ligne[0])
+    #print("Candidat", index-1 ,"=", listCandidat1)
+    #print("Candidat", index,"=", listCandidat2)
     resultat = 0
-    for element in listCandidat1:
+    for chiffre in listCandidat2:
         try:
-            resultat += int(element)
+            resultat += int(chiffre)
         except:
             pass
     if (resultat > 0):
-        print("resultat1 = ", resultat)
+        print("candidat", index," = ", resultat, "vs ", end="")
     resultat = 0
-    for element in listCandidat2:
+    for chiffre in listCandidat1:
         try:
-            resultat += int(element)
+            resultat += int(chiffre)
         except:
             pass
     if (resultat > 0):
-        print("resultat 2 = ", resultat)
-    listCandidat2.clear()
+        print("candidat",index-1, " = ", resultat)
     listCandidat1.clear()
+    listCandidat2.clear()
